b8a20b8ad50185a1972e6c827a1fd65e5413deb3
Kyle Lee <js66lee@uwaterloo.ca>
2023-10-15 23:50:43 -0400

    style: Format code according to flake8

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,13 +17,17 @@ from django.contrib import admin
 from django.urls import path
 from drf_spectacular.views import (
     SpectacularAPIView,
-    # SpectacularRedocView,
     SpectacularSwaggerView,
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
-    path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
-    # path('api/redoc/', SpectacularRedocView.as_view(url_name='api-schema'), name='api-redoc'),
+    path('api/schema/',
+         SpectacularAPIView.as_view(),
+         name='api-schema'
+         ),
+    path('api/docs/',
+         SpectacularSwaggerView.as_view(url_name='api-schema'),
+         name='api-docs'
+         ),
 ]
