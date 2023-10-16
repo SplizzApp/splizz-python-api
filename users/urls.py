57ff20c78276a09c58f2651fc339538d9c11114a
Kyle Lee <js66lee@uwaterloo.ca>
2023-10-15 23:54:01 -0400

    chore: remove unnecessary files

--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from users.views import UserDetailView, UserListView, UserCreateView
-
-urlpatterns = [
-    path('', UserListView.as_view(), name='user-list'),
-    path('new/', UserCreateView.as_view(), name='user-create'),
-    path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-]
