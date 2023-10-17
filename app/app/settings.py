cbf51d0f764bc283962c85b2ef4c381941986cba
Kyle Lee <js66lee@uwaterloo.ca>
2023-10-17 00:08:25 -0400

    feat: add user & user auth API implementation

--- app/app/settings.py
+++ app/app/settings.py
@@ -36,8 +36,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',  # 'rest_framework_simplejwt
     'drf_spectacular',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
